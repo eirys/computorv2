a5e8f7435a1b97d8758ebe309d07fdabfb711787
myhuong <etran@student.42.fr>
2023-01-06 22:48:58 +0100

    Added functions in Rational

--- incs/types/rational.hpp
+++ incs/types/rational.hpp
@@ -6,7 +6,7 @@
 /*   By: eli <eli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 13:34:17 by eli               #+#    #+#             */
-/*   Updated: 2023/01/06 22:07:19 by eli              ###   ########.fr       */
+/*   Updated: 2023/01/06 22:46:15 by eli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,26 @@ class Rational {
 	public:
 		Rational();
 		explicit Rational(long double x);
-		Rational(const std::string&& buf);
 		Rational(const Rational& x);
+		Rational(const std::string&& buf);
 		virtual ~Rational();
 		Rational&	operator=(const Rational& rhs);
+		Rational&	operator=(long double rhs);
 
 		long double	getVal() const;
 
+		operator long double() const;
+		
 	private:
 		long double	_val;
 };
 
 bool			operator==(const Rational& x, const Rational& y);
+bool			operator<(const Rational& x, const Rational& y);
+bool			operator!=(const Rational& x, const Rational& y);
+bool			operator<=(const Rational& x, const Rational& y);
+bool			operator>(const Rational& x, const Rational& y);
+bool			operator>=(const Rational& x, const Rational& y);
 
 std::ostream&	operator<<(std::ostream& o, const Rational& x);
 
--- src/rational.cpp
+++ src/rational.cpp
@@ -6,7 +6,7 @@
 /*   By: eli <eli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 13:42:10 by eli               #+#    #+#             */
-/*   Updated: 2023/01/06 22:07:13 by eli              ###   ########.fr       */
+/*   Updated: 2023/01/06 22:46:41 by eli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ Rational::Rational(long double x):
 Rational::Rational(const std::string&& s):
 	_val(std::stold(s)) {}
 
-Rational::~Rational() {}
-
 Rational::Rational(const Rational& x):
 	_val(x.getVal()) {}
+
+Rational::~Rational() {}
 
 Rational& Rational::operator=(const Rational& rhs) {
 	if (this == &rhs)
@@ -33,13 +33,50 @@ Rational& Rational::operator=(const Rational& rhs) {
 	return *this;
 }
 
+Rational& Rational::operator=(long double rhs) {
+	if (this->getVal() == rhs)
+		return *this;
+	_val = rhs;
+	return *this;
+}
+
+Rational::operator long double() const {
+	return _val;
+}
+
+/* Getters ******************************************/
+
 long double Rational::getVal() const {
 	return _val;
 }
 
+/* Relational operators *****************************/
+
 bool operator==(const Rational& x, const Rational& y) {
 	return x.getVal() == y.getVal();
 }
+
+bool operator<(const Rational& x, const Rational& y) {
+	return x.getVal() < y.getVal();
+}
+
+bool operator!=(const Rational& x, const Rational& y) {
+	return !operator==(x, y);
+}
+
+bool operator>(const Rational& x, const Rational& y) {
+	return !operator<=(x, y);
+}
+
+bool operator<=(const Rational& x, const Rational& y) {
+	return operator==(x, y) || operator<(x, y);
+}
+
+bool operator>=(const Rational& x, const Rational& y) {
+	return !operator<(x, y);
+}
+
+/* I/O stream operator *****************************/
 
 std::ostream&	operator<<(std::ostream& o, const Rational& x) {
 	o << x.getVal();
